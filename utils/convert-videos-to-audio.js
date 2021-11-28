eb6a367820d00cb319eec4f9b57ebf9c31d65883
nguyendacthienngan <nguyendacthienngan@gmail.com>
2021-11-28 12:51:21 +0700

    Detect video contains audio or not

--- utils/convert-videos-to-audio.js
+++ utils/convert-videos-to-audio.js
@@ -1,17 +1,31 @@
 const ffmpeg = require('fluent-ffmpeg');
 
-function videosConvertToAudio(input, output, callback) {
-     ffmpeg(input)
-        .output(output)
-        .on('end', function() {                    
-            console.log('conversion ended');
-            callback(null);
-        })
-        .on('error', function(err){
-            console.log('error: ', err);
-            callback(err);
-        }).run();
-   
+async function videosConvertToAudio(input, output, callback) {
+    let isAudioIncluded = false;
+    isVideoHaveAudioTrack(input, function (result) {
+        isAudioIncluded = result;
+        if (isAudioIncluded) {
+            ffmpeg(input)
+            .output(output)
+            .on('end', function() {                    
+                console.log('conversion ended');
+                callback(null);
+            })
+            .on('error', function(err){
+                console.log('error: ', err);
+                callback(err);
+            }).run();
+        } else {
+            callback("Video doesn't have audio")
+        }
+    })
+    
 }
 
+function isVideoHaveAudioTrack(input, callback) {
+    ffmpeg(input).ffprobe(function(err, data) {
+        console.log(data.streams.length)
+        callback(data.streams.length > 1) 
+      });
+}
 exports.videosConvertToAudio = videosConvertToAudio
