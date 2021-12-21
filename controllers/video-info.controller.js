4a37f737048613303ff216368597ce39f3492ca2
nguyendacthienngan <nguyendacthienngan@gmail.com>
2021-12-22 01:31:10 +0700

    Fix error cannot update video's description

--- controllers/video-info.controller.js
+++ controllers/video-info.controller.js
@@ -55,6 +55,7 @@ exports.updateVideoInfo = function (req, res) {
             }
             else {
                 video.title = body.title ? body.title : video.title;
+                video.description = body.description ? body.description : video.description;
                 video.url = body.url ? body.url : video.url;
                 video.size = body.size ? body.size : video.size;
                 video.save();
