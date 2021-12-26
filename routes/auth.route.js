0fdbc33691a1895b646dff955273a094dadcbb38
nguyendacthienngan <nguyendacthienngan@gmail.com>
2021-12-26 18:02:59 +0700

    Refactor code

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,8 +1,5 @@
 const express = require('express');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
 const router = express.Router();
-const User = require('../models/user');
 const userController = require('../controllers/users.controller');
 
 router.post('/register', userController.createUser);
