4c07de4978a338b0bb11977c86457b51cbfcb8fa
nguyendacthienngan <nguyendacthienngan@gmail.com>
2021-12-31 14:55:16 +0700

    Change register validator

--- validations/auth.js
+++ validations/auth.js
@@ -3,6 +3,8 @@ const Joi = require('joi');
 const registerValidator = (data) => {
     const cutoffDate = new Date(Date.now() - (1000 * 60 * 60 * 24 * 365 * 18)); // go back by 21 years
     const rule = Joi.object({
+        first_name: Joi.string(),
+        last_name: Joi.string(),
         username: Joi.string().min(6).max(225).required(),
         email: Joi.string().min(6).max(225).required().email(),
         password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{6,20}$')).required(),
