636803c402b625796b77b8c2f058be567a4e47fb
tkc <chuongtangkhanh2104@gmail.com>
2022-04-24 12:30:37 +0700

    fix: adjust mongoUri

---
+++ Makefile
@@ -0,0 +1,2 @@
+deploy-dev:
+	caprover deploy -d -h https://captain.demo.o-redsky.xyz -a api -b main
--- configs/database.js
+++ configs/database.js
@@ -1,10 +1,16 @@
 const mongoose = require('mongoose');
 
-const { MONGO_URL, MONGO_DATABASE_NAME } = process.env;
+// const { MONGO_URL, MONGO_DATABASE_NAME } = process.env;
 // const mongoUri = `${MONGO_URL}/${MONGO_DATABASE_NAME}`;
+const { MONGO_USERNAME, MONGO_PASSWORD, MONGO_HOST, MONGO_PORT, MONGO_DATABASE_NAME, NODE_ENV } = process.env;
 
-// const mongoUri = 'mongodb://127.0.0.1:27017/watch-out-server';
-const mongoUri = 'mongodb://root:gu123451@srv-captain--mongodb:27017/watch-out-server'
+// const mongoUri = 'mongodb://root:gu123451@srv-captain--mongodb:27017/watch-out-server'
+
+let mongoUri;
+if (NODE_ENV === "development") {
+  mongoUri = 'mongodb://127.0.0.1:27017/watch-out-server';
+}
+else mongoUri = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DATABASE_NAME}`
 
 async function connectToMongoDb() {
   const connection = await mongoose.connect(mongoUri);
