c20834e8b5dc8055fc032c28a8757f66b318074b
nguyendacthienngan <nguyendacthienngan@gmail.com>
2022-05-01 21:36:34 +0700

    Revert change in verifyToken

--- middlewares/verifyToken.middleware.js
+++ middlewares/verifyToken.middleware.js
@@ -3,13 +3,13 @@ const jwt = require('jsonwebtoken');
 
 exports.requireAuth = function (req, res, next) {
 	const authorizationHeader = req.headers['authorization'];
-	const accessToken = authorizationHeader || authorizationHeader.split(' ')[1];
+	const accessToken = authorizationHeader && authorizationHeader.split(' ')[1];
 
 	try {
 		if (!accessToken)
 			return res.status(401).json('Access Denied');
 
-		const user = jwt.verify(authorizationHeader, process.env.TOKEN_SECRET);
+		const user = jwt.verify(accessToken, process.env.TOKEN_SECRET);
 		req.user = user;
 		next()
 	}
