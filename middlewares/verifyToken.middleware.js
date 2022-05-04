fb00411659a2b36b5434be82a061ba004f5c20e2
tkc <chuongtangkhanh2104@gmail.com>
2022-05-04 15:21:20 +0700

    chore: remove console.log

--- middlewares/verifyToken.middleware.js
+++ middlewares/verifyToken.middleware.js
@@ -3,7 +3,9 @@ const jwt = require('jsonwebtoken');
 
 exports.requireAuth = function (req, res, next) {
 	const authorizationHeader = req.headers['authorization'];
+	console.log({ authorizationHeader });
 	const accessToken = authorizationHeader && authorizationHeader.split(' ')[1];
+	console.log({ accessToken });
 
 	try {
 		if (!accessToken)
