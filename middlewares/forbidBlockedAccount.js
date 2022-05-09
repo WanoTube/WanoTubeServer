fdb1d298e9da4efd03edb4e45bf377ae2f482ecc
tkc <chuongtangkhanh2104@gmail.com>
2022-05-10 00:07:59 +0700

    feat: return autogenerated_thumbnails_list

--- middlewares/forbidBlockedAccount.js
+++ middlewares/forbidBlockedAccount.js
@@ -4,10 +4,11 @@ const { BlockedStatus } = require('../constants/user');
 exports.forbidBlockedAccount = async (req, res, next) => {
   const { channelId } = req.user;
   const foundAccount = await Account.findById(channelId);
+  console.log(foundAccount.blocked_status);
   if (foundAccount.blocked_status !== BlockedStatus.NONE) {
     return res.status(403).json({
       message: "Not allowed"
-    })
+    });
   }
   next();
 }
